27d456c71fc91357ca3f25a2ed51dd322ad92c0c
Allan Gondin <allangondin@gmail.com>
2020-06-19 00:58:47 -0300

    finish 1/4

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,16 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import authMiddlewares from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/user', UserController.store);
 
 routes.post('/session', SessionController.store);
+
+routes.use(authMiddlewares);
+
+routes.put('/user', UserController.update);
 
 export default routes;
